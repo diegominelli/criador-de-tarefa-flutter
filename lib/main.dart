2ce1053060eec386d1b9c4a048ba68316aa44751
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-02-19 19:45:09 -0300

    alterando o widget de Stateless para Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -38,9 +38,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String nome;
   const Task(this.nome, {super.key});
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -54,32 +61,46 @@ class Task extends StatelessWidget {
               color: Colors.blue,
               height: 140,
             ),
-            Container(
-              color: Colors.white,
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    color: Colors.black26,
-                    width: 72,
-                    height: 100,
+            Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                      ),
+                      // ignore: sized_box_for_whitespace
+                      Container(
+                        width: 200,
+                        child: Text(
+                          widget.nome,
+                          style: const TextStyle(
+                              fontSize: 24, overflow: TextOverflow.ellipsis),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            nivel++;
+                          });
+                          // ignore: avoid_print
+                          print(nivel);
+                        },
+                        child: const Icon(Icons.arrow_drop_up),
+                      )
+                    ],
                   ),
-                  // ignore: sized_box_for_whitespace
-                  Container(
-                    width: 200,
-                    child: Text(
-                      nome,
-                      style: const TextStyle(
-                          fontSize: 24, overflow: TextOverflow.ellipsis),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Icon(Icons.arrow_drop_up),
-                  )
-                ],
-              ),
+                ),
+                Text(
+                  'Nível: $nivel',
+                  style: const TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ],
             ),
           ],
         ),
