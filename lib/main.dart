8298b3e6e570b8bd73c9a47255a404b197568c4a
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-02-21 20:37:15 -0300

    criacao e desenvolvimento do formulario de nova tarefa

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/initial_screen.dart';
+import 'screens/form_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InitialScreen(),
+      home: const FormScreen(),
     );
   }
 }
