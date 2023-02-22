13bb23e8831c9aa346a7fa36aba38b64c291ddad
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-02-21 21:00:32 -0300

    criando os controllers para os campos do formulario

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -8,6 +8,10 @@ class FormScreen extends StatefulWidget {
 }
 
 class _FormScreenState extends State<FormScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController difficultyController = TextEditingController();
+  TextEditingController imageController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +32,7 @@ class _FormScreenState extends State<FormScreen> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
+                  controller: nameController,
                   textAlign: TextAlign.center,
                   decoration: const InputDecoration(
                       border: OutlineInputBorder(),
@@ -35,6 +40,43 @@ class _FormScreenState extends State<FormScreen> {
                       fillColor: Colors.white70,
                       filled: true),
                 ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: difficultyController,
+                  textAlign: TextAlign.center,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Dificuldade',
+                      fillColor: Colors.white70,
+                      filled: true),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: imageController,
+                  textAlign: TextAlign.center,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Imagem',
+                      fillColor: Colors.white70,
+                      filled: true),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // ignore: avoid_print
+                  print(nameController.text);
+                  // ignore: avoid_print
+                  print(
+                    int.parse(difficultyController.text),
+                  );
+                  // ignore: avoid_print
+                  print(imageController.text);
+                },
+                child: const Text('Adicionar!'),
               ),
             ],
           ),
