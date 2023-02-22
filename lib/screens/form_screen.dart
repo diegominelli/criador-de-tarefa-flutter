8640da646294341a217853d19db5d9334218678e
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-02-21 21:47:01 -0300

    tratando o erro da foto

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -28,6 +28,8 @@ class _FormScreenState extends State<FormScreen> {
             border: Border.all(width: 3),
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -56,6 +58,9 @@ class _FormScreenState extends State<FormScreen> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
+                  onChanged: (text) {
+                    setState(() {});
+                  },
                   controller: imageController,
                   textAlign: TextAlign.center,
                   decoration: const InputDecoration(
@@ -65,13 +70,38 @@ class _FormScreenState extends State<FormScreen> {
                       filled: true),
                 ),
               ),
+              Container(
+                height: 100,
+                width: 72,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    width: 2,
+                    color: Colors.blue,
+                  ),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    imageController.text,
+                    errorBuilder: (BuildContext context, Object exception,
+                        StackTrace? stackTrace) {
+                      return Image.asset('assets/images/nophoto.jpg');
+                    },
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
               ElevatedButton(
                 onPressed: () {
                   // ignore: avoid_print
                   print(nameController.text);
                   // ignore: avoid_print
                   print(
-                    int.parse(difficultyController.text),
+                    int.parse(
+                      difficultyController.text,
+                    ),
                   );
                   // ignore: avoid_print
                   print(imageController.text);
