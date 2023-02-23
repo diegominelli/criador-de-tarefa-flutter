d4e0a248a7369df9fbbdc919e9c36e2e208cd90e
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-02-23 18:46:09 -0300

    abrindo o banco de dados

---
+++ lib/data/database.dart
@@ -0,0 +1,8 @@
+import 'package:sqflite/sqflite.dart';
+// ignore: depend_on_referenced_packages
+import 'package:path/path.dart';
+
+Future<Database> getDatabase() async {
+  final String path = join(await getDatabasesPath(), 'task.db');
+  return openDatabase(path, onCreate: (db, version){db.execute(tabela)}, version: 1,);
+}
