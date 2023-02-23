a010fffbfbfd87a64d8cc8fa9226ff179b21cac1
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-02-23 20:55:15 -0300

    criando o objeto de acessos aos dados

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -1,3 +1,4 @@
+import 'package:primeiro_projeto_flutter/data/task_dao.dart';
 import 'package:sqflite/sqflite.dart';
 // ignore: depend_on_referenced_packages
 import 'package:path/path.dart';
@@ -7,18 +8,8 @@ Future<Database> getDatabase() async {
   return openDatabase(
     path,
     onCreate: (db, version) {
-      db.execute(tableSql);
+      db.execute(taskDao.tableSql);
     },
     version: 1,
   );
 }
-
-const String tableSql = 'CREATE TABLE $_tablename('
-    '$_name TEXT, '
-    '$_difficulty INTEGER, '
-    '$_image TEXT)';
-
-const String _tablename = 'taskTable';
-const String _name = 'name';
-const String _difficulty = 'difficulty';
-const String _image = 'image';
---
+++ lib/data/task_dao.dart
@@ -0,0 +1,18 @@
+import '../components/task.dart';
+
+// ignore: camel_case_types
+class taskDao {
+  static const String tableSql = 'CREATE TABLE $_tablename('
+      '$_name TEXT, '
+      '$_difficulty INTEGER, '
+      '$_image TEXT)';
+
+  static const String _tablename = 'taskTable';
+  static const String _name = 'name';
+  static const String _difficulty = 'difficulty';
+  static const String _image = 'image';
+
+  save(Task tarefa) async {}
+
+  findAll() async {}
+}
