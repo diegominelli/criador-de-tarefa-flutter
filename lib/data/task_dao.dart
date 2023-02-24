05dd36161511051514b5b17958e3a3397db9d2f4
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-02-24 15:23:10 -0300

    implementando o metodo find

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -24,9 +24,35 @@ class taskDao {
         await bancoDeDados.query(_tablename);
     // ignore: avoid_print
     print('Procurando dados no banco de dados... encontrado: $result');
-    return toList()
+    return toList(result);
   }
 
-  Future<List<Task>> find(String nomeDaTarefa) async {}
+  List<Task> toList(List<Map<String, dynamic>> mapaDeTarefas) {
+    // ignore: avoid_print
+    print('Convertendo to List:');
+    final List<Task> tarefas = [];
+    for (Map<String, dynamic> linha in mapaDeTarefas) {
+      final Task tarefa = Task(linha[_name], linha[_image], linha[_difficulty]);
+      tarefas.add(tarefa);
+    }
+    // ignore: avoid_print
+    print('Lista de Tarefas $tarefas');
+    return tarefas;
+  }
+
+  Future<List<Task>> find(String nomeDaTarefa) async {
+    // ignore: avoid_print
+    print(' Acessando find: ');
+    final Database bancoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> result = await bancoDeDados.query(
+      _tablename,
+      where: '$_name = ?',
+      whereArgs: [nomeDaTarefa],
+    );
+    // ignore: avoid_print
+    print('Tarefa encontrada: ${toList(result)}');
+    return toList(result);
+  }
+
   delete(String nomeDaTarefa) async {}
 }
